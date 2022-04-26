104b3604d1e039c6cdbc6c6543c3bb0f92c8e12a
Ekaterina <3022805283@qq.com>
2022-04-26 17:51:19 +0800

    JUnit4 version added

--- src/test/java/ru/netology/service/CashbackHackServiceTest2.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest2.java
@@ -1,27 +1,31 @@
 package ru.netology.service;
 
-import org.testng.annotations.Test;
+import org.junit.Test;
 
-import static org.testng.Assert.*;
+import static org.junit.Assert.*;
 
-public class CashbackHackServiceTest {
+public class CashbackHackServiceTest2 {
     CashbackHackService service = new CashbackHackService();
+    
     @Test
-        public void shouldReturnCorrectRemainWithCommonAmount() {
+    public void shouldReturnCorrectRemainWithCommonAmount() {
         int actual = service.remain(900);
         int expected = 100;
-        assertEquals(actual,expected);
+        assertEquals(expected,actual);
     }
+    
     @Test
     public void shouldReturn1000IfAmountIs0() {
         int actual = service.remain(0);
         int expected = 1000;
-        assertEquals(actual,expected);
+        assertEquals(expected,actual);
     }
+    
     @Test
     public void shouldReturn0IfAmountIsAMultipleOf1000() {
         int actual = service.remain(1000);
         int expected = 0;
-        assertEquals(actual,expected);
+        assertEquals(expected,actual);
     }
+    
 }
