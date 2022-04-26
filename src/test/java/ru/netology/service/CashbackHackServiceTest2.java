e73ae774e957f687634961bb53a9c867e0ce0c9f
Ekaterina <3022805283@qq.com>
2022-04-26 18:00:38 +0800

    Create gradle.yml

--- src/test/java/ru/netology/service/CashbackHackServiceTest2.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest2.java
@@ -11,21 +11,21 @@ public class CashbackHackServiceTest2 {
     public void shouldReturnCorrectRemainWithCommonAmount() {
         int actual = service.remain(900);
         int expected = 100;
-        assertEquals(expected,actual);
+        assertEquals(expected, actual);
     }
     
     @Test
     public void shouldReturn1000IfAmountIs0() {
         int actual = service.remain(0);
         int expected = 1000;
-        assertEquals(expected,actual);
+        assertEquals(expected, actual);
     }
     
     @Test
     public void shouldReturn0IfAmountIsAMultipleOf1000() {
         int actual = service.remain(1000);
         int expected = 0;
-        assertEquals(expected,actual);
+        assertEquals(expected, actual);
     }
     
 }
